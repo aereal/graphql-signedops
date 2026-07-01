6d876c3f0c38c9d8034523207945e281dbc49d6e
aereal <trasty.loose@gmail.com>
2026-07-01 20:17:34 +0900

    ci: setup GitHub Actions workflow

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vitest/config";
 
+const isInGHA = process.env.GITHUB_ACTIONS === "true";
+
 export default defineConfig({
   test: {
     projects: ["pkgs/*"],
     watch: false,
-    reporters: process.env.GITHUB_ACTIONS !== "" ? ["default", "github-actions"] : ["default"],
+    reporters: isInGHA ? ["default", "github-actions"] : ["default"],
+    coverage: {
+      provider: "v8",
+      include: ["pkgs/**/src/**/*.ts"],
+      reporter: isInGHA ? ["cobertura"] : ["html"],
+    },
   },
 });
