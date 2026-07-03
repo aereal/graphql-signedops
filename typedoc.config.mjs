d7d5024dd988a4c2455003309b0e2b7bdf178ee5
aereal <trasty.loose@gmail.com>
2026-07-03 10:12:39 +0900

    ci: injects build URL

--- typedoc.config.mjs
+++ typedoc.config.mjs
@@ -1,3 +1,12 @@
+const { job_html_url } = process.env;
+
+const navigationLinks = {
+  "GitHub Repo": "https://github.com/aereal/graphql-signedops",
+};
+if (job_html_url) {
+  navigationLinks["Build Job"] = job_html_url;
+}
+
 /**
  * @type {import('typedoc').TypeDocOptions}
  */
@@ -10,6 +19,7 @@ const config = {
   exclude: ["pkgs/test-utils"],
   out: "./docs",
   name: "graphql-signedops",
+  navigationLinks,
 };
 
 export default config;
